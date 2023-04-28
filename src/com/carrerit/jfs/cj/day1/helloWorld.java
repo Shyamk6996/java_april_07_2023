ead4772db24e77a5706fbad1aabac1a545363baa
shyamsundarkonda <kondashyamsundar6996@hmail.com>
2023-04-28 11:37:17 -0400

    added extra log info

--- src/com/carrerit/jfs/cj/day1/helloWorld.java
+++ src/com/carrerit/jfs/cj/day1/helloWorld.java
@@ -3,5 +3,9 @@ package com.carrerit.jfs.cj.day1;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("welcome to java world");
+        for(int i=1 ; i <= 5 ; i++)
+        {
+            System.out.println("welcome user : "+i);
+        }
     }
 }
